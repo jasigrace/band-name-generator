6028f05df5e2a566ffd7540be84560b6a38973cd
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-05-13 21:40:38 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome, Get The Excellent Name for your Bang Bang Band!!\n")
+city_name = input("Tell me the name of the city you grew up in.. \n")
+pet_name = input("Now, Tell me your pet's name..  \n")
+print("And your amazing band name could be " + city_name + " " + pet_name + "!")
